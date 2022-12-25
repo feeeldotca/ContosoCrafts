c82cba119a825aa2ec0b07381febacd49b0802d7
Kun Zhang <feel@shaw.ca>
2022-12-24 23:08:05 -0700

    Add project files.

---
+++ ContosoCrafts.Website/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using ContosoCrafts.Website.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            products = ProductService.GetProducts();
+        }
+    }
+}
