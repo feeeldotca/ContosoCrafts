9ae2c554b5fd01bd2a95a845b77f6be1620da022
Kun Zhang <feel@shaw.ca>
2022-12-25 23:53:25 -0700

    Added a simple API for products Json results

--- ContosoCrafts.Website/Program.cs
+++ ContosoCrafts.Website/Program.cs
@@ -1,4 +1,7 @@
+using ContosoCrafts.Website.Models;
 using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,5 +27,12 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+app.MapGet("/products", (context) =>
+{
+    var products = app.Services.GetService<JsonFileProductService>().GetProducts();
+   
+    var json = JsonSerializer.Serialize<IEnumerable<Product>>(products);
+    return context.Response.WriteAsync(json);
+});
 
 app.Run();
